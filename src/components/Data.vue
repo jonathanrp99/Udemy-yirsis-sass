b5be04dab838d852d203cac7d623a9baf6f541e2
jonathan Ramos Pinel <jonyrampi@gmail.com>
2022-09-14 22:46:42 +0200

    finished the section

---
+++ src/components/Data.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="info" style="margin-left: 5em;">
+    <h2>Info...</h2>
+    <h3>
+     <span class="crypto">BTC</span> 
+       -
+    <span class="coin">USD</span>
+    </h3>
+    <img src="https://www.cryptocompare.com/media/37746238/eth.png"  alt="">
+    <p>
+      Price: <strong>$ 300</strong>
+    </p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+
+</style>
---
+++ src/components/Form.vue
@@ -0,0 +1,45 @@
+<template>
+    <div class="">
+      <form>
+        <div>
+          <label>
+            National currency: 
+          </label>
+          <select>
+            <option value="" disabled selected>
+              Select a coin
+            </option>
+            <option>EUR</option>
+            <option>USD</option>
+            <option>MXN</option>
+          </select>
+        </div>
+        <div>
+          <label>
+            Crypto currency: 
+          </label>
+          <select>
+            <option value="" disabled selected>
+              Select a coin
+            </option>
+            <option>BTC</option>
+            <option>ETH</option>
+            <option>LTC</option>
+          </select>
+        </div>
+        <div>
+          <button type="submit" style="margin-top: 4em;">
+            Quotate
+          </button>
+        </div>
+      </form>
+    </div>
+  </template>
+  
+  <script setup>
+  
+  </script>
+  
+  <style>
+  
+  </style>
